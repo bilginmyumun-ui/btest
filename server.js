cd6be3378098b6ab5303328e4896a83df0df1f7a
bilginmyumun-ui <bilgin.myumun@etisoftware.com>
2026-05-12 09:50:02 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,31 +1,13 @@
-{
-  "type": "INTERACTION",
-  "status": "SUCCEEDED",
-  "data": {
-    "appId": "D48WpcgssldD",
-    "interactionId": "qZi2hXy9QQJFFpyAESHJF",
-    "interactions": [
-      {
-        "type": "SHOW",
-        "id": "qZi2hXy9QQJFFpyAESHJF",
-        "slate": {
-          "rootBlock": "root",
-          "blocks": [
-            {
-              "id": "root",
-              "name": "Container",
-              "props": "{}",
-              "children": "[\"iframe-child\"]"
-            },
-            {
-              "id": "iframe-child",
-              "name": "Iframe",
-              "props": "{\"src\":\"https://aliyapp.azurewebsites.net/gateway/embed?blockKey=chat&actorId=StWLB81F0U\",\"height\":720}",
-              "children": "[]"
-            }
-          ]
-        }
-      }
-    ]
-  }
-}
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("OK");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Running on " + PORT);
+});
