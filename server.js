6664f7dff8b951dfe986810e080ff287a16df06a
bilginmyumun-ui <bilgin.myumun@etisoftware.com>
2026-05-12 09:51:30 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,47 @@ import express from "express";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("OK");
+app.use(express.json());
+
+app.post("/interaction", (req, res) => {
+  res.json({
+    type: "INTERACTION",
+    status: "SUCCEEDED",
+    data: {
+      appId: "D48WpcgssldD",
+      interactionId: "qZi2hXy9QQJFFpyAESHJF",
+      interactions: [
+        {
+          type: "SHOW",
+          id: "qZi2hXy9QQJFFpyAESHJF",
+          slate: {
+            rootBlock: "root",
+            blocks: [
+              {
+                id: "root",
+                name: "Container",
+                props: "{}",
+                children: "[\"iframe-child\"]"
+              },
+              {
+                id: "iframe-child",
+                name: "Iframe",
+                props: JSON.stringify({
+                  src: "https://aliyapp.azurewebsites.net/gateway/embed?blockKey=chat&actorId=StWLB81F0U",
+                  height: 720
+                }),
+                children: "[]"
+              }
+            ]
+          }
+        }
+      ]
+    }
+  });
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log("Running on " + PORT);
+  console.log("App running on " + PORT);
 });
